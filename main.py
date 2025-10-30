f8957518e1ee721b2f419d7c47c69322facff5aa
Anuj kumar sharma <anujsharmagbn11@gmail.com>
2025-10-30 21:25:44 +0530

    Modify EURI_API_KEY loading method
    Updated EURI_API_KEY retrieval to support Streamlit secrets.

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from app.ui import pdf_uploader
 from app.files_utils import extract_text_from_pdf , extract_text_from_csv, extract_text_from_docx, extract_text_from_excel,extract_text_from_txt
 from app.vectorstore_utils import create_faiss_index,retrive_relevant_docs
 from app.chat_utils import get_chat_model, ask_chat_model
-from app.config import EURI_API_KEY
+#from app.config import EURI_API_KEY
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import time
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import re
+import os
+import streamlit as st
+
+# Try to load key from Streamlit secrets (for deployment)
+EURI_API_KEY = st.secrets.get("EURI_API_KEY") or os.getenv("EURI_API_KEY")
+
 
 
 
